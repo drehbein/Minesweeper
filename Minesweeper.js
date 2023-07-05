f55e3979b8c7e859b30837272a687adffb47f8b6
drehbein <devionrehbein@gmail.com>
2023-07-05 02:08:28 -0500

    Added in-game click event logic

--- Minesweeper.js
+++ Minesweeper.js
@@ -1,8 +1,8 @@
 function setBoard() {
 	// take user input
-	var rows = parseInt(document.getElementById("rows").value);
-	var columns = parseInt(document.getElementById("columns").value);
-	var mines = parseInt(document.getElementById("mines").value); 
+	rows = parseInt(document.getElementById("rows").value);
+	columns = parseInt(document.getElementById("columns").value);
+	mines = parseInt(document.getElementById("mines").value); 
 	if (rows > 0 && columns > 0 && mines > 0) {
 		document.getElementById("boardTitle").innerHTML = "A " + rows + "x" + columns + " board with " + mines + " mines!";
 	} else {
@@ -14,7 +14,7 @@ function setBoard() {
 	document.getElementById("boardArea").innerHTML = area;
 
 	// generate mine locations using a Durstenfeld shuffle
-	const cells = Array.from({length: area}, (_, i) => i + 1);
+	const cells = Array.from({length: area}, (_, i) => i);
 	for (var i = cells.length - 1; i > 0; i--) {
 		var j = Math.floor(Math.random() * (i+1));
 		var temp = cells[i];
@@ -25,18 +25,48 @@ function setBoard() {
 	document.getElementById("mineLocations").innerHTML = mineLocations;
 
 	// create an array without mines
-	const gameState = [];
+	gameState = [];
 	while (gameState.length < area) {
-		gameState.push("0");
+		gameState.push(0);
 	}
 
 	// add each mine
 	for (const mineLocation of mineLocations) {
-		gameState[mineLocation - 1] = 4;
+		gameState[mineLocation] = 4;
 	}
 	document.getElementById("gameState").innerHTML = gameState;
 
+	renderBoard();
+}
 
+function clickEvent(event, cell) {
+	if (event.shiftKey) {
+		toggleFlag(cell);
+	} else {
+		reveal(cell);
+	}
+}
+
+function toggleFlag(cell) {
+	var cellState = gameState[cell];
+	if (cellState == 0 || cellState == 4) {
+		gameState[cell] = cellState + 1;
+		renderBoard();
+	} else if (cellState == 1 || cellState == 5) {
+		gameState[cell] = cellState - 1;
+		renderBoard();
+	}
+}
+
+function reveal(cell) {
+	var cellState = gameState[cell];
+	if (cellState == 0 || cellState == 4) {
+		gameState[cell] = cellState + 2;
+		renderBoard();
+	}
+}
+
+function renderBoard() {
 	// remove old board
 	if (x = document.getElementById("board")) {
 		x.remove();
@@ -48,7 +78,7 @@ function setBoard() {
 	document.getElementById("boardCont").appendChild(board);
 
 	// reset cell ticker
-	var cellCount = 1;
+	var cellCount = 0;
 
 	// add rows/cells
 	for (let i = 0; i < rows; i++) {
@@ -60,12 +90,14 @@ function setBoard() {
 			var cell = document.createElement("TD");
 			cell.setAttribute("id",`Cell ${cellCount}`); 
 			var button = document.createElement("BUTTON");
-			button.innerHTML = gameState[cellCount - 1];
+			button.innerHTML = gameState[cellCount];
+			button.setAttribute("onclick", `clickEvent(event, ${cellCount})`);
 			cell.appendChild(button);
 			row.appendChild(cell);
 			cellCount++;
 		}
 	}
+
 }
 
 function toggleDebugging() {
@@ -76,4 +108,8 @@ function toggleDebugging() {
 	} else {
 		debugging.setAttribute("hidden", "hidden");
 	}
+}
+
+function test() {
+	reveal(1);
 }
