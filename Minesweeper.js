4c45aa5680ef1185cad0622a312920d4c3c0558b
drehbein <devionrehbein@gmail.com>
2023-07-04 22:40:58 -0500

    Added cell labels

--- Minesweeper.js
+++ Minesweeper.js
@@ -1,4 +1,5 @@
 function setBoard() {
+	//take user input
 	var rows = parseInt(document.getElementById("rows").value);
 	var columns = parseInt(document.getElementById("columns").value); 
 	if (rows > 0 && columns > 0) {
@@ -7,14 +8,20 @@ function setBoard() {
 		document.getElementById("boardTitle").innerHTML = "Positive numbers only please!"
 	}
 
+	//remove old boards
 	if (x = document.getElementById("board")) {
 		x.remove();
 	}
 
+	//create table
 	var board = document.createElement("TABLE");
 	board.setAttribute("id", "board");
 	document.getElementById("boardCont").appendChild(board);
-	
+
+	//reset cell ticker
+	var cellCount = 1;
+
+	//Add rows/columns
 	for (let i = 0; i < rows; i++) {
 		var row = document.createElement("TR");
 		row.setAttribute("id", "row");
@@ -22,9 +29,10 @@ function setBoard() {
 
 		for (let j = 0; j < columns; j++) {
 			var cell = document.createElement("TD");
-			var text = document.createTextNode("text node");
+			var text = document.createTextNode(`Cell ${cellCount}`);
 			cell.appendChild(text);
 			row.appendChild(cell);
+			cellCount++;
 		}
 	}
 }
